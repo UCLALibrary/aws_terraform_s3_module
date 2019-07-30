558532b24b7ac574a4f5fecc35dcc747b6635b10
Anthony <vuonga91@gmail.com>
2019-07-30 11:54:14 -0700

    Add name output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "bucket_arn" {
   value = "${aws_s3_bucket.bucket.arn}"
 }
 
+output "bucket_name" {
+  value = "${aws_s3_bucket.bucket.name}"
+}
+
 output "bucket_region" {
   value = "${aws_s3_bucket.bucket.region}"
 }
