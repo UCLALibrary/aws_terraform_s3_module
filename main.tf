39c4f7e44c2b870da8b8fc6a8c7449637d7560ac
Anthony <vuonga91@gmail.com>
2019-08-09 14:31:42 -0700

    Add force_destroy flag

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.bucket_name}"
-  region = "${var.bucket_region}"
-  acl    = "private"
+  bucket        = "${var.bucket_name}"
+  region        = "${var.bucket_region}"
+  force_destroy = "${var.force_destroy_flag}"
+  acl           = "private"
 }
 
